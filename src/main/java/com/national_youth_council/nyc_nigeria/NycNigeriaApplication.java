9205cb6141e0d98edc38ab98daafa5edd33f3a84
= <=>
2025-10-30 09:50:13 +0100

    project initialization

---
+++ src/main/java/com/national_youth_council/nyc_nigeria/NycNigeriaApplication.java
@@ -0,0 +1,13 @@
+package com.national_youth_council.nyc_nigeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycNigeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NycNigeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/national_youth_council/nyc_nigeria/NycNigeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.national_youth_council.nyc_nigeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NycNigeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
